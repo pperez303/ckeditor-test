bc6f82b05bdf0f8c21ac444ab8f410535f83b717
pperez303 <ipeteperez@hotmail.com>
2023-10-14 10:30:49 -0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {CKEditor} from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Using CKEditor 5 build in React</h2>
+      <CKEditor 
+        editor={ ClassicEditor }
+        data="<p>Hello from CKEditor 5!</p>"
+        onInit={ editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log( 'Editor is ready to use!', editor );
+        } }
+        config={{
+          ckfinder: { uploadUrl: "http://localhost:8000/api/ckloads/uploads" },
+          types: ['png', 'jpeg'],
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
